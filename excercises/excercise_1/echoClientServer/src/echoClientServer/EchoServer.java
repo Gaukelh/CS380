536676adade759ecb92667aa2c3e31df098221f6
Hunter Gaukel <gaukelh@gmail.com>
2017-09-27 15:15:23 -0700

    Socket gets closed when thread is started....!

--- excercises/excercise_1/echoClientServer/src/echoClientServer/EchoServer.java
+++ excercises/excercise_1/echoClientServer/src/echoClientServer/EchoServer.java
@@ -17,28 +17,39 @@ public final class EchoServer {
                     //connection setup
                     String address = socket.getInetAddress().getHostAddress();
                     System.out.printf("Client connected: %s%n", address);
-                    //output streams
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
                     
-                    //input streams
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-                    BufferedReader br = new BufferedReader(isr);
-                    
-                    String input;
-                    while((input = br.readLine()) != null) {
-                        out.printf("Server> %s%n", input);
-                        if(input.equals("exit")){
-                            break;
+                    Runnable ser = () -> {
+                        try {
+                            System.out.println(":)");
+                            //output streams
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            System.out.println(":)");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
+
+                            //input streams
+                            InputStream is = socket.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                            BufferedReader br = new BufferedReader(isr);
+
+                            String input;
+                            while((input = br.readLine()) != null) {
+                                out.printf("Server> %s%n", input);
+                                if(input.equals("exit")){
+                                    break;
+                                }
+                            }
+                            //close down the connections
+                            socket.close();
+                            os.close();
+                            is.close();
+                            System.out.printf("Client disconnected: %s%n", address);
+                        } catch (Exception e) {
+                            System.out.println(e);
                         }
-                    }
-                    //close down the connections
-                    socket.close();
-                    os.close();
-                    is.close();
-                    System.out.printf("Client disconnected: %s%n", address);
+                    };
+                    Thread serThread = new Thread(ser);
+                    serThread.start();
                 } catch (Exception e) {
                     System.out.println("Error or Client Disconnected");
                 }
